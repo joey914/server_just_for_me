f9a68b36f7506e6b47265f62f6ff53ccfa4ceeef
joey914 <joey91426@gmail.com>
2017-03-13 01:18:16 +0900

    example finished
    studied how to use

--- 정보수집.js
+++ 정보수집.js
@@ -4,19 +4,21 @@ $(document).ready(function() {
         var kt = $("#kt").val();
         var pn = $("#pn").val();
         var email = $("#email").val();
-        var nsc = $("#nsc").val();
-        var wsc = $("#wsc").val();
         var btg = $("#bt").val();
 
 
-            var regdata = "name=" + name + "&kakaotalk=" + kt + "&Phonenum=" + pn +"&email=" + email + "&Wantscore=" + wsc +"&Nowscore=" + nsc + "&battletag=" + btg;
+            var regdata = "name=" + name + "&kakaotalk=" + kt + "&Phonenum=" + pn +"&email=" + email + "&battletag=" + btg;
+            alert("이까진잘딤");
             $.ajax({        
                 type: "POST",
-                url: "http://soylatte.kr:3000/bill",
+                url: "http://joey914.iptime.org:3000/register",
                     data: regdata,
                     success: function(data)     {         //data - response from server
                     if (data.success == true) {
                         alert("정상적으로 접수되었습니다.");
+                    }
+                    else{
+                      alert("데이타석세스를못받음");
                     }
                 },
                     error: function(xhr, status, error)     {
